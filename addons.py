ebc6da8d640dd638f91571678cdbcf86c5cc7466
Libor Zoubek <lzoubek@redhat.com>
2015-02-04 20:16:24 +0100

    forgot ,

--- addons.py
+++ addons.py
@@ -23,7 +23,7 @@ __ADDONS__ = [
     "plugin.video.zkouknito.cz",
     "script.module.stream.resolver",
     "repository.xbmc.doplnky",
-    "repository.dmd-xbmc.googlecode.com"
+    "repository.dmd-xbmc.googlecode.com",
     "repository.kodi-czsk"
     ] + _BEAM_
 
