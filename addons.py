ebee9acc1da16d82de19eaea53607749363c1aa5
Libor Zoubek <lzoubek@redhat.com>
2015-03-14 21:14:37 +0100

    start releasing sledujuserialy.cz

--- addons.py
+++ addons.py
@@ -17,6 +17,7 @@ __ADDONS__ = [
     "plugin.video.pohadkar.cz",
     "plugin.video.rtvs.sk",
     "plugin.video.sosac.ph",
+    "plugin.video.sledujuserialy.cz",
     "plugin.video.ta3.com",
     "plugin.video.teevee.sk",
     "plugin.video.tv.sosac.ph",
