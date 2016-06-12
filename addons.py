091abb1fc73de8274e14c736c83506fe3626acae
jose1711 <jose1711@gmail.com>
2016-06-12 22:37:19 +0200

    adoptovany a fixnuty doplnok pre huste.tv

--- addons.py
+++ addons.py
@@ -44,7 +44,8 @@ __ADDONS__ = [
     "repository.dmd-xbmc.googlecode.com",
     "repository.kodi-czsk",
     "weather.dmd-xbmc.in-pocasi",
-    "plugin.video.mojetv-archiv"
+    "plugin.video.mojetv-archiv",
+    "plugin.video.huste.tv"
     ] + _BEAM_
 
 import os
