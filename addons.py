ec0fb8a2d46878e68d616898e6114a4a9e4a9f34
Michal Paulovic <coolmip@gmail.com>
2017-04-03 00:42:48 +0200

    movie scraper (#25)
    * metadata.bbaron

--- addons.py
+++ addons.py
@@ -47,6 +47,7 @@ __ADDONS__ = [
     "repository.kodi-czsk",
     "weather.dmd-xbmc.in-pocasi",
     "plugin.video.dmd-czech.huste",
+    "metadata.bbaron",
     "plugin.video.stream-cinema"
     ] + _BEAM_
 
